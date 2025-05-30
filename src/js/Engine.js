c693803e928b4130653d6a9ea6982191790246de
Gaurab Khanal <khanalgaurab98@gmail.com>
2025-05-30 16:30:48 +0545

    feat: Functionality complete

--- src/js/Engine.js
+++ src/js/Engine.js
@@ -48,7 +48,12 @@ export class Engine {
         const currentFloorIndex = track[i][`lift_` + i].split("_")[1];
         track[i][`lift_` + i] = `floor_` + floor;
         console.log(track[i]);
-        this.moveLiftAfterFoorUpdate(i, currentFloorIndex, floor);
+        const isMoved = this.movementUpDown(i, currentFloorIndex, floor, fn);
+        if (!isMoved) {
+          track[i].run = false;
+          track[i][`lift_` + i] = `floor_` + currentFloorIndex;
+          continue;
+        }
         break;
       }
     }
@@ -70,9 +75,28 @@ export class Engine {
     });
   }
 
-  async moveLiftAfterFoorUpdate(liftIndex, currentFloorIndex, nextFloorIndex) {
-    // get floor height
+  // fn for finding which btn(up/down) is clicked and move down with down btn and up with up btn
+  movementUpDown(liftIndex, currentFloorIndex, nextFloorIndex, btn) {
+    const floorToMove = Math.abs(currentFloorIndex - nextFloorIndex);
+    // calculate lift movement down or up if floorToMove is -ve then only down btn should move
+    // lift down and if +ve then only up can move lift up.
+    const movement = currentFloorIndex - nextFloorIndex;
+    console.log("Movement : ", movement);
+    if (floorToMove == 0) return false;
+    if (movement < 1 && btn == "down") {
+      this.moveLiftAfterFoorUpdate(liftIndex, floorToMove, nextFloorIndex);
+      return true;
+    } else if (movement >= 1 && btn == "up") {
+      this.moveLiftAfterFoorUpdate(liftIndex, floorToMove, nextFloorIndex);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async moveLiftAfterFoorUpdate(liftIndex, floorToMove, nextFloorIndex) {
     await this.animateDoorOnLiftCall(liftIndex);
+    // get floor height
     const floor = document.getElementsByClassName("floor")[0];
     const floorHeight = floor.getBoundingClientRect().height;
 
@@ -84,12 +108,14 @@ export class Engine {
     // calculate distn between adjacent floor (bottom of one floor to another )
     const calculetedDistn = floorHeight + floorGap;
     const pixelToMove = (this.noOfFloor - nextFloorIndex - 1) * calculetedDistn;
-
+    console.log("Pixel to move: ", pixelToMove);
     const getLiftToMove = document.getElementById(`lift_${liftIndex}`);
-    const floorToMove = currentFloorIndex - nextFloorIndex;
+    console.log("FloorTo MOve: ", floorToMove);
     getLiftToMove.style.transition = `transform ${floorToMove * 2}s linear`;
     getLiftToMove.style.transform = `translateY(-${pixelToMove}px)`;
-    setTimeout(() => {
+
+    setTimeout(async () => {
+      await this.animateDoorOnLiftCall(liftIndex);
       this.trackLiftMapFloor[liftIndex].run = false;
     }, floorToMove * 2 * 1000);
     console.log(this.trackLiftMapFloor[liftIndex]);
